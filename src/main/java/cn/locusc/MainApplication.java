4fcd3aeace492d1b6529d73f516bd2ceed0bf7e6
locusc <2227628925@qq.com>
2024-09-23 12:02:16 +0800

    v-master nfs and pv pvc

---
+++ src/main/java/cn/locusc/MainApplication.java
@@ -0,0 +1,18 @@
+package cn.locusc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author locusc
+ * k8s练习应用
+ * 2024/9/21
+ */
+@SpringBootApplication
+public class MainApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+    
+}
