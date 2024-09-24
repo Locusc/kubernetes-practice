1d378d0a18298e85c862a89d5f31568117c5e560
locusc <2227628925@qq.com>
2024-09-24 10:20:47 +0800

    v-master jenkins images

--- src/main/resources/jenkins/start.sh
+++ src/main/resources/jenkins/start.sh
@@ -1,4 +1,9 @@
+# docker.sock 此映射卷主要用于执行docker镜像内的docker命令执行 共享当前docker的状态
+
 docker run -itd --name jenkins -p 8080:8080 -p 50000:50000 -u root \
 -e JAVA_OPTS=-Duser.timezone=Asia/Shanghai --restart=always \
--v /data/jenkins/:/var/jenkins_home/ \
-192.168.59.3:5000/public/jenkins/jenkins:2.401.3-lts-alpine
+-v /app/jenkins/data:/var/jenkins_home/ \
+-v /usr/bin/docker:/usr/bin/docker \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v /etc/docker/daemon.json:/etc/docker/daemon.json \
+locusc/jenkins:2.401.3-lts-alpine
