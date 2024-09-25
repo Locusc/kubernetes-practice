40940351b9307f2bebd11169556bcb4e036f24a9
locusc <2227628925@qq.com>
2024-09-25 10:27:32 +0800

    v-master update PracticeController.java

--- src/main/java/cn/locusc/controller/PracticeController.java
+++ src/main/java/cn/locusc/controller/PracticeController.java
@@ -1,7 +1,9 @@
 package cn.locusc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PracticeController {
 
     @RequestMapping("/")
